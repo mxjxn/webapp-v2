f6796eac1aa3d8ea5fdb81b60c7b37d0b9436877
MXJXN <modulations@gmail.com>
2023-04-24 16:05:36 -0400

    fixing default colormode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
-import { THEME_INITIAL_COLOR } from 'utils/config'
+import { THEME_INITIAL_COLOR_MODE } from 'utils/config'
 
 export default function Document() {
   return (
@@ -10,7 +10,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
-        <ColorModeScript initialColorMode={THEME_INITIAL_COLOR} />
+        <ColorModeScript initialColorMode={THEME_INITIAL_COLOR_MODE} />
         <Main />
         <NextScript />
       </body>
